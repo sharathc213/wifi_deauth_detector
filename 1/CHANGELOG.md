93a09a5c1a9d6ee2b6135042d319714647d1d1db
sharath <csharath213@gmail.com>
2022-10-16 09:12:22 -0400

    first commit

---
+++ 1/CHANGELOG.md
@@ -0,0 +1,3 @@
+# CryptoDash - Free Cryptocurrency Dashboard Template
+
+V1.0 : [20-08-2021] :  Initial Release
---
+++ 1/src/js/scripts/documentation.js
@@ -0,0 +1,13 @@
+/*=========================================================================================
+	File Name: documentation.js
+	Description: Theme documentation js file
+	----------------------------------------------------------------------------------------
+	Item Name: CryptoDash - Free Cryptocurrency Dashboard Template
+	Version: 1.0
+	Author: ThemeSelection
+	Author URL: https://themeselection.com/
+==========================================================================================*/
+
+$(document).ready(function(){
+   $('body').scrollspy({ target: '#sidebar-page-navigation' });
+});
